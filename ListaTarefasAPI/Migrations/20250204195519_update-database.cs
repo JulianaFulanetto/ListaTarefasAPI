ef11bfb21b266d413479f95eeb2a85f361d38f81
Juliana <juliana.fulanetto@aluno.senai.br>
2025-02-04 16:56:32 -0300

    Inclusão de banco de dados

---
+++ ListaTarefasAPI/Migrations/20250204195519_update-database.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ListaTarefasAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TiposTarefa",
+                columns: table => new
+                {
+                    TipoTarefaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TiposTarefa", x => x.TipoTarefaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tarefas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Concluida = table.Column<bool>(type: "bit", nullable: false),
+                    TipoTarefaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarefas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tarefas_TiposTarefa_TipoTarefaId",
+                        column: x => x.TipoTarefaId,
+                        principalTable: "TiposTarefa",
+                        principalColumn: "TipoTarefaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tarefas_TipoTarefaId",
+                table: "Tarefas",
+                column: "TipoTarefaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tarefas");
+
+            migrationBuilder.DropTable(
+                name: "TiposTarefa");
+        }
+    }
+}
